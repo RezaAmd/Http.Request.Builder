72a12d7201b54de118ced6ec64f336e706d8f34d
Reza Ahmadi <Rezaahmadidvlp@gmail.com>
2024-05-20 12:03:22 +0330

    🧹 Refactor test.

--- tests/HttpRequest.Console/Program.cs
+++ tests/HttpRequest.Console/Program.cs
@@ -1,13 +1,13 @@
 ﻿using Http.Request.Builder.Builder;
 
-InputData inputData = new()
+UserInputData user = new()
 {
     Username = "RezaAmd",
     Password = "123456"
 };
 
 var request = RequestBuilder.Create("https://localhost:44383/authentication/signIn", HttpMethod.Post)
-    .WithDataFromBodyAsJson(inputData)
+    .WithDataFromBodyAsJson(user)
     .WithHeader("CUSTOM_HEADER", "YOUR_API_KEY")
     .WithRetryAttemptsForFailed(5)
     .Build();
@@ -17,7 +17,7 @@ var response = await request.SendAsync();
 Console.WriteLine($"{response.StatusCode} {response.Content}");
 
 
-public class InputData
+public class UserInputData
 {
     public string Username { get; set; }
     public string Password { get; set; }
